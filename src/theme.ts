474c1f28dafa5ddd8eb98c57ae1f32d1744d5730
hansol <hansol8701@naver.com>
2024-01-12 21:55:05 +0900

    챕터 3

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+    bgColor: "white",
+    textColor: "black",
+    btnColor: "black",
+};
+
+export const darkTheme: DefaultTheme = {
+    bgColor: "black",
+    textColor: "white",
+    btnColor: "white",
+};
